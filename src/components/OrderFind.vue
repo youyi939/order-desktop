d6b3d4ef5e3de22e45fd39d4eb10f6b0d9e88193
youyi <46435808+youyi939@users.noreply.github.com>
2020-10-17 11:30:29 +0800

    订单查询系统0.1

---
+++ src/components/OrderFind.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h1>订单页面查询</h1>
+        <li v-for="item in list" :key="item.id">
+            {{"订单ID："+item.id+ "，订单信息："+item.message}}
+        </li>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "OrderFind",
+        data(){
+          return{
+              list:[]
+          }
+        },
+
+        methods:{
+            getData(){
+                fetch('http://127.0.0.1:7001/findAll',{method:'GET'})
+                    .then(resp=>resp.json())
+                    .then(albums=> this.list = albums)
+            }
+         }
+
+        ,
+        mounted() {
+            this.getData();
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
+import OrderFind from "@/components/OrderFind";
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(OrderFind),
 }).$mount('#app')
